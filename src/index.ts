6f10181298b559b79cb48aee788362d6a1705980
f <eribra@kth.se>
2022-12-19 23:05:42 +0100

    started implementing spatial grid

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,37 @@
  * GridMap - Spatial grid that extends Map
  */
 export default class GridMap extends Map {
-    constructor() {
+    #cells: [][][] = [];
+    #cellSize = 1;
+    constructor(w: number, h: number, cellSize: number) {
         super();
+        for (let y = 0; y < h / cellSize; y++) {
+            this.#cells[y] = [];
+            for (let x = 0; x < w / cellSize; x++) {
+                this.#cells[y][x] = [];
+            }
+        }
     }
-    set(key: any, value: any) {
-        super.set(key, value);
-        return this;
+    set(id: any, value: any, x = 0, y = 0) {
+
+
+        return super.set(id, value);
+    }
+    delete(id: any) {
+
+
+        return super.delete(id);
+    }
+    update(id: any, x: number, y: number) {
+
+    }
+    getIdsInView(rx: number, ry: number, rw: number, rh: number): any[] {
+        const ids: any[] = [];
+        for (let y = ry; y < (ry + rh) / this.#cellSize; y++) {
+            for (let x = rx; x < (rx + rw) / this.#cellSize; x++) {
+                ids.push(this.#cells[y][x]);
+            }
+        }
+        return ids;
     }
 }
