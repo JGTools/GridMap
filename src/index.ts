6d7b5882c3015a7e510bd66b0694f02be05619ac
f <eribra@kth.se>
2022-12-21 13:04:14 +0100

    added documentation

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ import SHG from "@jgtools/shg";
  */
 export default class GridMap<K, V> extends Map {
     #shg;
+
+    /**
+     * @param w width of grid
+     * @param h height of grid
+     * @param cs cellSize
+     */
     constructor(w: number, h: number, cs: number) {
         super();
         this.#shg = new SHG<K>(w, h, cs);
@@ -20,6 +26,13 @@ export default class GridMap<K, V> extends Map {
     update(id: K, x: number, y: number) {
         this.#shg.update(id, x, y);
     }
+
+    /**
+     * @param x 
+     * @param y 
+     * @param range 
+     * @returns list of ids in range
+     */
     query(x: number, y: number, range: number): K[] {
         return this.#shg.query(x, y, range);
     }
